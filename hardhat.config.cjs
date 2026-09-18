36ea50cdc961caad90a2c07976e216333ec172d5
SAKSHAM463 <sakshamsahoo25@gmail.com>
2026-09-18 07:44:39 +0530

    Add files via upload

--- hardhat.config.cjs
+++ hardhat.config.cjs
@@ -1,5 +1,3 @@
-require("@nomicfoundation/hardhat-toolbox");
-
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
   solidity: {
@@ -14,18 +12,22 @@ module.exports = {
   },
   networks: {
     hardhat: {
+      type: "edr-simulated",
       chainId: 31337,
     },
     localhost: {
+      type: "http",
       url: "http://127.0.0.1:8545",
       chainId: 31337,
     },
     baseSepolia: {
+      type: "http",
       url: process.env.BASE_SEPOLIA_RPC_URL || "https://sepolia.base.org",
       accounts: process.env.PRIVATE_KEY ? [process.env.PRIVATE_KEY] : [],
       chainId: 84532,
     },
     arbitrumSepolia: {
+      type: "http",
       url: process.env.ARBITRUM_SEPOLIA_RPC_URL || "https://sepolia-rollup.arbitrum.io/rpc",
       accounts: process.env.PRIVATE_KEY ? [process.env.PRIVATE_KEY] : [],
       chainId: 421614,
